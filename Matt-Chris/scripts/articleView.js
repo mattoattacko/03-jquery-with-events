6dde0d84766de5d3738026ba9126c285887dd881
Chris Kozlowski <kozlowskicd@gmail.com>
2018-07-31 21:31:39 -0700

    work on functions

--- Matt-Chris/scripts/articleView.js
+++ Matt-Chris/scripts/articleView.js
@@ -39,9 +39,11 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
+      
       // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      $('#articles').hide();
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('#articles').fadeIn(1000);  //BETA CODE
     } else {
       // TODO: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
 
@@ -78,4 +80,5 @@ articleView.setTeasers = function() {
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
   articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
