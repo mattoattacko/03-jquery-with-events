c8e1ea41c2c03b63a6e8be57d506da7012e77f6b
Chris Kozlowski <kozlowskicd@gmail.com>
2018-08-04 10:41:58 -0700

    work on filter functions

--- Matt-Chris/scripts/articleView.js
+++ Matt-Chris/scripts/articleView.js
@@ -75,7 +75,7 @@ articleView.handleCategoryFilter = function() {
   // When an option with a value is selected, hide all the articles, then reveal the matches.
   // When the blank (default) option is selected, show all the articles, except for the template.
   // Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
 };
 
 articleView.handleMainNav = function() {
